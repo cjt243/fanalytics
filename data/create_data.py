a5d6111bb9e02697c4478a6eb052a29ad6cdcaaa
Cullin Tripp <cullin.tripp@snowflake.com>
2022-02-09 15:24:47 -0500

    shelving the multi-page concept for now

--- data/create_data.py
+++ data/create_data.py
@@ -5,3 +5,7 @@ def create_table(n=7):
     df = pd.DataFrame({"x": range(1, 11), "y": n})
     df['x*y'] = df.x * df.y
     return df
+
+def get_yahoo_fantasy(session):
+     df = session.execute('select * from basketball.yahoo.yahoo_fantasy;').fetch_pandas_all()
+     return df
